4fd2d352c9870347d79f9e84b6d95a423aecdf55
kamadatakumi <yuuteayuu335dat@gmail.com>
2017-07-07 15:54:57 +0900

    Scene作成2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	//次のシーンを指定する。空の場合は何もしない。
+	public static string NextScene = "";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.O)){
+			NextScene = "GameOver";
+	}
+		else if(Input.GetKeyDown(KeyCode.C)){
+			NextScene = "Clear";
+		}
+		if(NextScene.Length > 0){
+			SceneManager.LoadSceneAsync (NextScene,LoadSceneMode.Additive);
+			NextScene = "";
+			enabled = false;
+		}
+}
+}
---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		//Fire1キーが押されたら、Gameシーンに切り替え
+		if(Input.GetButtonDown("Fire1")){
+			SceneManager.LoadSceneAsync("Game");
+		}
+	}
+}
